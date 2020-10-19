6a710232cdb62281115cc32c5a54f9f8763c5a1a
loganlil1 <sli2@loyalty.com>
2020-10-19 15:00:26 -0400

    Add example tests

---
+++ src/components/shared/header/Header.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render,unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import Header from '../../../components/shared/header/Header'
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('Should render header', async () => {
+
+    act(() => {
+        render(<Header title="test" />, container);
+      });
+      expect(container.textContent).toBe("test");
+  })
---
+++ src/components/shared/linkButton/LinkButton.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render,unmountComponentAtNode } from "react-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { act } from "react-dom/test-utils";
+import LinkButton from '../../../components/shared/linkButton/LinkButton'
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test('Should render LinkButton', async () => {
+
+    act(() => {
+        render(<Router><LinkButton text="Test Link" href="/test" /></Router>, container);
+      });
+      expect(container.textContent).toBe("Test Link");      
+  }
+)
